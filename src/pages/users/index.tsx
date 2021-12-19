80993ca6ac372f5e116743ced36ab1770ed723a0
Daniel <daniel.pin@outlook.com>
2021-12-19 01:19:57 -0300

    Responsividade listagem usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,79 +1,145 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { Sidebar } from "../../components/Sidebar";
 import { Header } from "../../components/Header";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination";
 
-export default function UserList(){
-    return (
-        <Box>
-            <Header />
+export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
 
-             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-                <Sidebar />
+  return (
+    <Box>
+      <Header />
 
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-                    <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
 
-                        <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20"/>}>Criar Novo</Button>
-                    </Flex>                        
-                        <Table>
-                            <Thead>
-                                <Tr>
-                                    <Th px="6" color="gray.300" width="8"> 
-                                        <Checkbox colorScheme="pink"/>
-                                    </Th>
-                                    <Th>Usuário</Th>
-                                    <Th>Data de cadastro</Th>
-                                    <Th width="8"></Th>
-                                </Tr>
-                            </Thead>
-                            <Tbody>
-                                <Tr>
-                                    <Td px="6">
-                                        <Checkbox colorScheme="pink"/>
-                                    </Td>
-                                    <Td>
-                                        <Box>
-                                            <Text fontWeight="bold">Daniel Pinheiro do Nascimento</Text>
-                                            <Text fontSize="sm" color="gray.300">daniel.pin@outlook.com</Text>
-                                        </Box>
-                                    </Td>
-                                    <Td> 18 de Dezembro, 2021</Td>
-                                    <Td><Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}>Editar</Button></Td>
-                                </Tr>
-                                <Tr>
-                                    <Td px="6">
-                                        <Checkbox colorScheme="pink"/>
-                                    </Td>
-                                    <Td>
-                                        <Box>
-                                            <Text fontWeight="bold">Daniel Pinheiro do Nascimento</Text>
-                                            <Text fontSize="sm" color="gray.300">daniel.pin@outlook.com</Text>
-                                        </Box>
-                                    </Td>
-                                    <Td> 18 de Dezembro, 2021</Td>
-                                    <Td><Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}>Editar</Button></Td>
-                                </Tr>
-                                <Tr>
-                                    <Td px="6">
-                                        <Checkbox colorScheme="pink"/>
-                                    </Td>
-                                    <Td>
-                                        <Box>
-                                            <Text fontWeight="bold">Daniel Pinheiro do Nascimento</Text>
-                                            <Text fontSize="sm" color="gray.300">daniel.pin@outlook.com</Text>
-                                        </Box>
-                                    </Td>
-                                    <Td> 18 de Dezembro, 2021</Td>
-                                    <Td><Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}>Editar</Button></Td>
-                                </Tr>
-                            </Tbody>
-                        </Table>       
-                        <Pagination/>            
-                </Box>
-             </Flex>
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar Novo
+            </Button>
+          </Flex>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Daniel Pinheiro do Nascimento</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      daniel.pin@outlook.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td> 18 de Dezembro, 2021</Td>}
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Daniel Pinheiro do Nascimento</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      daniel.pin@outlook.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td> 18 de Dezembro, 2021</Td>}
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Daniel Pinheiro do Nascimento</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      daniel.pin@outlook.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td> 18 de Dezembro, 2021</Td>}
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+          <Pagination />
         </Box>
-    )
+      </Flex>
+    </Box>
+  );
 }
